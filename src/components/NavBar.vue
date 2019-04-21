ef0213312749a8310d095983c1c3523c03bfbd8f
Javier Blasco Isaza <jblascoisa@icloud.com>
2019-04-21 23:18:37 +0200

    navbar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="light" variant="light">
+      <b-navbar-brand to="/">Tego Dashboard</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/">Home</b-nav-item>
+          <b-nav-item to="/about">About</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
