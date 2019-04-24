9f677ef2ca2c84bcb04bd5ab986cdc2be16fd289
Javier Blasco Isaza <jblascoisa@icloud.com>
2019-04-24 11:48:33 +0200

    added jumbotron component to home view

---
+++ src/components/General/JumboTron.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <b-jumbotron :header="header" :lead="lead">
+      <p>{{ pContent }}</p>
+      <b-button variant="primary" href="#">{{ bContent }}</b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    header: String,
+    lead: String,
+    pContent: String,
+    bContent: String
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,19 @@
 <template>
-  <div>
-    <h1>Home</h1>
-  </div>
+  <JumboTron
+    header="Tego"
+    lead="Bootstrap 4 Components for Vue.js 2"
+    pContent="For more information visit website"
+    bContent="More Info"
+  />
 </template>
 
 <script>
+import JumboTron from '@/components/General/JumboTron.vue';
+
 export default {
-  name: 'home'
+  name: 'home',
+  components: {
+    JumboTron
+  }
 };
 </script>
