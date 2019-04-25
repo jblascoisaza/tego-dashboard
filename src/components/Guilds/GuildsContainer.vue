a9537881ddf4737fdcddc0c417ef08f050bd6c35
Javier Blasco Isaza <jblascoisa@icloud.com>
2019-04-25 13:27:34 +0200

    separated guilds lists via guildscontainer component

---
+++ src/components/Guilds/GuildsContainer.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <b-container class="bv-example-row" align="center">
+      <b-row>
+        <b-col v-for="(guild, index) in paginatedItems" :key="index">
+          <GuildCard
+            :name="guild.name"
+            :iconURL="
+              guild.iconURL
+                ? guild.iconURL
+                : 'https://cdn.discordapp.com/icons/467300599885594624/1aede8acaf8cac9f70a51eb905b1fe4c.png'
+            "
+            :guildID="guild.id"
+          />
+        </b-col>
+      </b-row>
+      <b-pagination
+        @change="onPageChanged"
+        v-model="currentPage"
+        :per-page="perPage"
+        :total-rows="rows"
+        align="center"
+      ></b-pagination>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import GuildCard from '@/components/Guilds/GuildCard.vue';
+
+export default {
+  components: {
+    GuildCard
+  },
+  props: {
+    guildsList: Array
+  },
+  data() {
+    return {
+      perPage: 3,
+      currentPage: 1,
+      paginatedItems: {}
+    };
+  },
+  methods: {
+    paginate(page_size, page_number) {
+      this.paginatedItems = this.guildsList.slice(
+        page_number * page_size,
+        (page_number + 1) * page_size
+      );
+    },
+    onPageChanged(page) {
+      this.paginate(this.perPage, page - 1);
+    }
+  },
+  computed: {
+    rows() {
+      return this.guildsList.length;
+    },
+    pageCount() {
+      const l = this.guildsList.length;
+      const s = this.perPage;
+      return Math.floor(l / s);
+    }
+  },
+  mounted() {
+    this.paginate(this.perPage, 0);
+  }
+};
+</script>
--- src/views/Guilds.vue
+++ src/views/Guilds.vue
@@ -3,46 +3,11 @@
     <b-card no-body>
       <b-tabs small card>
         <b-tab title="Standard">
-          <b-container class="bv-example-row" align="center">
-            <b-row>
-              <b-col v-for="(guild, index) in paginatedItems" :key="index">
-                <GuildCard
-                  :name="guild.name"
-                  :iconURL="
-                    guild.iconURL
-                      ? guild.iconURL
-                      : 'https://cdn.discordapp.com/icons/467300599885594624/1aede8acaf8cac9f70a51eb905b1fe4c.png'
-                  "
-                  :guildID="guild.id"
-                />
-              </b-col>
-            </b-row>
-            <b-pagination
-              @change="onPageChanged"
-              v-model="currentPage"
-              :per-page="perPage"
-              :total-rows="rows"
-              align="center"
-            ></b-pagination>
-          </b-container>
+          <GuildsContainer :guildsList="canManageGuilds" />
         </b-tab>
         <b-tab title="Premium Plan">
           <p v-if="!premiumGuilds.length">No premium guilds.</p>
-          <b-container v-else class="bv-example-row" align="center">
-            <b-row>
-              <b-col v-for="(guild, index) in premiumGuilds" :key="index">
-                <GuildCard
-                  :name="guild.name"
-                  :iconURL="
-                    guild.iconURL
-                      ? guild.iconURL
-                      : 'https://cdn.discordapp.com/icons/467300599885594624/1aede8acaf8cac9f70a51eb905b1fe4c.png'
-                  "
-                  :guildID="guild.id"
-                />
-              </b-col>
-            </b-row>
-          </b-container>
+          <GuildsContainer v-else :guildsList="premiumGuilds" />
         </b-tab>
       </b-tabs>
     </b-card>
@@ -51,50 +16,20 @@
 
 <script>
 import { mapState } from 'vuex';
-import GuildCard from '@/components/Guilds/GuildCard.vue';
+import GuildsContainer from '@/components/Guilds/GuildsContainer.vue';
 
 export default {
   name: 'guilds',
   components: {
-    GuildCard
+    GuildsContainer
   },
-  data() {
-    return {
-      perPage: 3,
-      currentPage: 1,
-      paginatedItems: {}
-    };
-  },
-  methods: {
-    paginate(page_size, page_number) {
-      this.paginatedItems = this.canManageGuilds.slice(
-        page_number * page_size,
-        (page_number + 1) * page_size
-      );
-    },
-    onPageChanged(page) {
-      this.paginate(this.perPage, page - 1);
-    }
-  },
-  computed: {
-    rows() {
-      return this.canManageGuilds.length;
-    },
-    pageCount() {
-      const l = this.canManageGuilds.length;
-      const s = this.perPage;
-      return Math.floor(l / s);
-    },
-    ...mapState('oAuth', ['canManageGuilds', 'premiumGuilds'])
-  },
-  mounted() {
-    this.paginate(this.perPage, 0);
-  }
+  computed: mapState('oAuth', ['canManageGuilds', 'premiumGuilds'])
 };
 </script>
 
 <style scoped>
 .guilds {
   padding: 4rem 2rem;
+  background-color: #e9ecef;
 }
 </style>
