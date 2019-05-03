7d7bba9bf84dc6fbb590eb7002803795deddc146
Javier Blasco Isaza <jblascoisa@icloud.com>
2019-05-03 13:58:19 +0200

    vue-fontawesome

--- src/main.js
+++ src/main.js
@@ -2,13 +2,20 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store/store';
+
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-import NProgress from 'nprogress';
+
 import 'nprogress/nprogress.css';
+import NProgress from 'nprogress';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 Vue.use(BootstrapVue);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.config.productionTip = false;
 
 router.beforeEach((to, from, next) => {
@@ -24,6 +31,8 @@ router.beforeEach((to, from, next) => {
 
 export const PBar = NProgress;
 PBar.configure({ showSpinner: false });
+
+library.add(faCoffee);
 
 new Vue({
   router,
